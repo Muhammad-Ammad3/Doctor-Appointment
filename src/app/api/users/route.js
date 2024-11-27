1982d3f2e7d6f27fffe17318e39671570491842f
Muhammad-Ammad3 <ammad0598@gmail.com>
2024-11-27 16:29:15 +0500

    commit

---
+++ src/app/api/users/route.js
@@ -0,0 +1,42 @@
+import { connectDB } from "@/lib/connectDB"
+import { UserModel } from "@/lib/models/UserModel"
+
+
+export async function POTS(req){
+    await connectDB();
+ try {
+    const obj = await req.json()
+    let newUser = await new UserModel({ ...obj })
+    newUser = await newUser.save()
+    return  Response.json({
+        error: false,
+        msg: "User Register Successfully",
+        user: newUser,
+    },{
+        status: 201
+    })
+ } catch (error) {
+    return  Response.json({
+        error: true,
+        msg: "Something Went Wrong",
+    },{
+        status: 400
+    })
+ }
+}
+
+export async function GET(req){
+    await connectDB();
+    const users = await UserModel.find();
+    return  Response.json({
+        error: false,
+        msg: "User Fetched Successfully",
+        users,
+    },{
+        status: 200
+    })
+}
+
+export async function PUT(req){}
+
+export async function DELETE(req){}
--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose"
 
 
 
-
-
 export async function  connectDB(){
     let connection
-    connection = await mongoose.connect('')
+    if(connection?.connection?.readyState != 1){
+        connection = await mongoose.connect(process.env.MONGODB_URI)
+        console.log('DB connect successfully');
+        
+    }
 }
